3197cab39eb91b35f437af4b84a917329cd6e3f1
amadzai <20966134+amadzai@users.noreply.github.com>
2026-05-10 18:32:16 +0800

    chore: add scroll restoration config

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import './styles/index.css';
 
+if ('scrollRestoration' in history) {
+  history.scrollRestoration = 'manual';
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
